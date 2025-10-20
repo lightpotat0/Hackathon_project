a7cddc536e3b5c85148e76a3f93d34f1efaea22c
lightpotat0 <lightvaz@gmail.com>
2025-10-20 14:08:47 -0300

    updates perfil

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -9,27 +9,27 @@ export default function Perfil() {
   const points = "1240.00";
 
   return (
-    <div className="min-h-screen bg-[var(--background)] text-[var(--foreground)] flex flex-col p-10">
+    <div className="min-h-screen bg-[var(--background)] text-[var(--foreground)] flex flex-col pt-10">
 
-      <div className="flex justify-start gap-10 w-[80%]">
-        <div className="w-[60%] bg-white/10 p-6 rounded-xl shadow-lg ml-[5%]">
-          <h2 className="text-2xl text-center font-bold mb-4">
+      <div className="flex justify-start w-[100%]">
+        <div className="w-[30%] bg-white/10 p-6 rounded-xl shadow-lg">
+          <h2 className="text-2xl text-center font-bold">
             Badges & Conquistas
           </h2>
-          <div className="grid grid-cols-2 gap-5">
+          <div className="grid grid-cols-2 gap-3 mt-10 cursor-pointer">
             {[
-              { name: "Contribuidor", icon: "🏅", desc: "Participou de projetos" },
-              { name: "Top 10", icon: "🔥", desc: "Entre os 10 melhores do ranking" },
-              { name: "Mentor", icon: "🎓", desc: "Ajudou novos membros" },
-              { name: "Ativo", icon: "⚡", desc: "Acessou 7 dias seguidos" },
-              { name: "Veterano", icon: "🕰️", desc: "Mais de 6 meses na plataforma" },
-              { name: "Explorador", icon: "🌎", desc: "Visitou todas as seções" },
+              { name: "Contribuidor", icon: "🏅" },
+              { name: "Top 10", icon: "🔥" },
+              { name: "Mentor", icon: "🎓" },
+              { name: "Ativo", icon: "⚡"},
+              { name: "Veterano", icon: "🕰️"  },
+              { name: "Ver mais" },
             ].map((badge, index) => (
               <div
                 key={index}
-                className="flex flex-col items-center justify-center bg-white/20 border border-white/30 rounded-xl p-4 hover:scale-105 transition-all"
+                className="flex flex-row items-center bg-white/20 border border-white/10 rounded-xl p-1 hover:scale-105 transition-all"
               >
-                <div className="text-4xl mb-2">{badge.icon}</div>
+                <div className="text-4xl">{badge.icon}</div>
                 <h3 className="text-lg font-semibold">{badge.name}</h3>
                 <p className="text-xs text-gray-300 text-center mt-1">{badge.desc}</p>
               </div>
@@ -37,18 +37,21 @@ export default function Perfil() {
           </div>
         </div>
 
-        {/* Coluna Direita — Perfil */}
-        <div className="w-[70%] ml-10 bg-white/10 p-6 rounded-xl shadow-lg flex flex-row items-center justify-center mb-10">
+        <div className="w-[100%] bg-white/10 rounded-xl shadow-lg flex flex-row items-center justify-center">
           <img
             src="https://cdn-icons-png.flaticon.com/512/0/93.png"
             alt="Ícone de usuário"
-            className="w-52 h-52 border border-black rounded-full object-cover mb-5"
+            className="w-52 h-52 border border-black rounded-full object-cover"
           />
-          <h1 className="text-6xl font-bold text-center mb-3 ml-10">{username}</h1>
+          <h1 className="text-6xl font-bold text-center ml-10">{username}</h1>
           <div className="text-center border border-black rounded-lg w-70 p-2 ml-14">
             <h1 className="text-5xl font-bold">Pontuação</h1>
             <p className="text-4xl font-bold">{points}</p>
           </div>
+        </div>
+        <div>
+          <hq>Currículo</hq>
+          <p>Meu nome é Rodrigo</p>
         </div>
       </div>
 
